714d79bf06d90f7308bb14f25099a7b1f3966683
Sangit91 <125936723+Sangit91@users.noreply.github.com>
2023-02-22 14:47:39 +0700

    add music

---
+++ playsong.js
@@ -0,0 +1,23 @@
+(function () {
+
+    function playMusic(){
+      var myAudio = document.getElementById('playAudio');
+      if (myAudio.duration > 0 && !myAudio.paused) {
+          //Its playing...do your job
+
+      } else {
+        myAudio.play()
+        //Not playing...maybe paused, stopped or never played.
+
+      }
+    }
+    var body = document.getElementsByTagName("body")[0];
+    body.addEventListener("click",function(ev){
+        
+        playMusic()
+      })
+    
+    
+    }
+    
+    )
